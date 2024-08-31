425bb1bfee50a29a4f2d1c27ee5dbdc3e5e264a3
Ricardo Lima <ricardolimaa65@gmail.com>
2024-08-31 18:08:47 -0300

    Co-authored-by: pedrofoschi <pedrofoschi@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import customtkinter as ctk
-
+import time
 
 janela = ctk.CTk()
 # Configrações do aplicativo
@@ -8,14 +8,21 @@ janela.title("UploadFiles with Python")
 # Tamanho do App
 janela.geometry("1280x700")#Tamanho do app
 # Tema
-janela._set_appearance_mode('dark-green')
+janela._set_appearance_mode('dark')
 # Icone
 # Trava maxix e mini
 janela.resizable(False,False)
 # Frames
 ############################################################################ Perfil
+
+
 frame_perfil = ctk.CTkFrame(janela,width=250,height=700).place(x=0,y=0)
 text1 = ctk.CTkLabel(frame_perfil,text='Perfil',bg_color='transparent').place(x=5,y=5)
+
+
+
+
+
 
 
 ############################################################################ Área de upload
@@ -28,6 +35,7 @@ text2 = ctk.CTkLabel(frame_principal,text='Área de upload').place(x=750,y=0)
 
 exibir_arquivo = ctk.CTkEntry(frame_principal, width=730, height=30).place(x=265, y=60)
 selecao_arquivos = ctk.CTkButton(frame_principal, text='Selecione o arquivo...', width= 200,height=30).place(x=1000, y=60)
+
 
 
 
